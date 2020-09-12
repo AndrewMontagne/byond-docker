a472198a0c7e9bc927195e8d3093b14c4aac2c57
Andrew O'Rourke <andrew@montagne.uk>
2020-09-12 22:25:01 +0100

    Added .gitlab-ci.yml

--- inode64/build.sh
+++ inode64/build.sh
@@ -26,4 +26,5 @@ echo "" | gcc -xc -fPIC -shared -o b64/inode64.so -
 # ld will pickup 64b lib for 64bit apps and 32b lib with fixed stat for 32bit apps
 
 cp b64/inode64.so /lib/x86_64-linux-gnu/inode64.so
+cp b32/inode64.so /lib/i386-linux-gnu/inode64.so
 cp b32/inode64.so /lib/inode64.so
